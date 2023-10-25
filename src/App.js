24167d12494a9024e043d99b82c0f6403bf00ae3
elyton pereira <fernandeselyton@gmail.com>
2023-10-24 23:43:39 -0300

    regressive-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [totalTimeInSeconds, setTotalTimeInSeconds] = useState(5)
+  const minutes = Math.floor(totalTimeInSeconds / 60);
+  const seconds = Math.floor(totalTimeInSeconds % 60);
+
+  useEffect(() =>{
+    setTimeout(() => {
+      if(totalTimeInSeconds === 0){
+        alert('O tempo acabou')
+        return
+      }
+      setTotalTimeInSeconds(totalTimeInSeconds -1)
+    }, 1000);
+    
+  }, [totalTimeInSeconds]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{minutes.toString().padStart(2, "0")}</span>
+      <span>:</span>
+      <span>{seconds.toString().padStart(2, "0")}</span>
     </div>
   );
 }
